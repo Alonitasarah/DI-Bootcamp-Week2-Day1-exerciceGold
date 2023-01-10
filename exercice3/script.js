96ad449f08393a8a7827d204957ef9b0ec73d564
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-10 18:38:39 +0000

    Add files via upload

---
+++ exercice3/script.js
@@ -0,0 +1,15 @@
+let num1 = prompt("Entrez le premier nombre");
+  console.log(typeof(num1));
+  let num2 = prompt("Entrez un second nombre");
+  sum = parseInt(num1) + parseInt(num2);
+  alert(sum);
+
+
+  sub = parseInt(num1) - parseInt(num2);
+  alert(sub);
+  
+  multi = parseInt(num1) * parseInt(num2);
+  alert(multi);
+  
+  div = parseInt(num1) / parseInt(num2);
+  alert(div);
