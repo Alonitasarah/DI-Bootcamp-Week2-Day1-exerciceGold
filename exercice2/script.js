d88138373480f749c5925bf145033c67e49459b8
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-10 18:35:34 +0000

    Add files via upload

---
+++ exercice2/script.js
@@ -0,0 +1,9 @@
+let firstVariable = "la plus belle ";
+let secondVariable = "Sarah";
+
+console.log(firstVariable.slice(0,2));
+console.log(secondVariable.slice(0,2));
+
+let thirdVarible = firstVariable.concat(secondVariable)
+
+console.log(thirdVarible);
